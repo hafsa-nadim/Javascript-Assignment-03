be88363a38873419cc50c8740b4230f15a3e3146
Sheikh Hafsa Nadeem <124384550+hafsa-nadim@users.noreply.github.com>
2023-05-13 21:00:12 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+    //               ================Assignment # 03=================
+
+    //               Question:02
+    // let age=18;
+    // alert("I am "+age+" years old.");
+             
+    //               Question:02
+    // let visitsNum = 0;
+    // let i = 0;
+    // for(i=0;i<1000;i++){
+    //   visitsNum = visitsNum+1;
+    //   alert("You have visited this site " + visitsNum + " times.")
+    //  }
+                 //  Question:03
+    // let birthday = 2004;
+    // document.write("My birth year is " + birthday );
+    // document.write("<br>");
+    // document.write("Data type of my declared variable is number");
+               
+                //    Question:04
+//     let clientName = "Hafsa Nadim";
+//     let orders = 10;
+//     let productTitle = "Shirt";
+
+//    document.write( "<b>" + clientName + "</b>"+ " ordered " + "<b>"+ orders  + " "+ productTitle +  "</b>(s)"+ "     on XYZ Clothing store");            
